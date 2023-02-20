4a3ea80cf87f8060fc5db76430770b920baf05c2
Rekha <rekha7.p@gmail.com>
2023-02-20 12:53:43 +1100

    commit changes

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const router = express.Router();
 
 // Hello World for index page
 app.get('/', function (req, res) {
-    return res.send("Hello World!");
+    return res.send("Hello Welcome to Node Js App for Azure DevOps!");
 })
 
 app.get('/api', function (req, res) {
